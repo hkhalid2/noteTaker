34bbbfe64ad70028754b19e2ebb84796924e62cf
Hamza Khalid <hamzer95@gmail.com>
2021-08-16 14:28:01 -0400

    heroku testing

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const uuid = require('./helpers/uuid')
 const { readFromFile, readAndAppend } = require('./helpers/fsUtil')
 const path = require('path');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 
@@ -12,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
-// GET Route for homepage
+// GET Route homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
