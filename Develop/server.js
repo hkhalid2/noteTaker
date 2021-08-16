29c42ec78b049ca656d5e90359c3282c014d487d
Hamza Khalid <hamzer95@gmail.com>
2021-08-15 22:15:10 -0400

    added additional GET routes as well as listener to show app functioning

--- Develop/server.js
+++ Develop/server.js
@@ -2,19 +2,43 @@ const express = require('express');
 const fs = require('fs');
 const util = require('util');
 const uuid = require('./helpers/uuid')
-const {}
+const { readFromFile, writeToFile, readAndAppend} = require('./helpers/fsUtil')
 const path = require('path');
-// const { clog } = require('./middleware/clog');
+
 const PORT = process.env.port || 3001;
 const app = express();
 
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 // GET Route for notes
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
-// GET Route for feedback page
+// GET Route for index html file
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+// GET Route for reading db.json and return all saved notes.
+app.get('/api/notes', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+
+
+
